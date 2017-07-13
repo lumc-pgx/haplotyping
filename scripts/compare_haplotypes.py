8835170e332f8bb5f391c041217b13fdcff42ba2
Guy Allard <w.g.allard@lumc.nl>
2017-07-13 15:20:08 +0200

    indicate significant variants

--- scripts/compare_haplotypes.py
+++ scripts/compare_haplotypes.py
@@ -130,13 +130,13 @@ def tabulate_allele_matches(allele, match):
     
     hits = match["haplotypes"]
     columns = []
-    columns.append(["{:25}".format("Variant|Haplotype")] + \
+    columns.append(["{:25}".format("Variant")] + \
                    ["{:25}".format(v) for v in match["known_variants"]] + \
                    ["{:25}".format(label) for label in ["Fraction", "Proportion", "Jaccard", "Significant"]])
-    
+
     for hit in hits:
         column = ["{:6}".format(hit["haplotype"])]
-        column += ["{:6}".format("*" if v in hit["haplotype_variants"] else " ") for v in match["known_variants"]]
+        column += ["{:6}".format("$" if v in match["significant_variants"] and v in hit["haplotype_variants"] else ("*" if v in hit["haplotype_variants"] else "")) for v in match["known_variants"]]
         column.append("{:6}".format("{}/{}".format(len(hit["shared_variants"]), len(hit["haplotype_variants"]))))
         column.append("{:6}".format(str(hit["fraction"])[:4]))
         column.append("{:6}".format(str(hit["jaccard"])[:4]))
