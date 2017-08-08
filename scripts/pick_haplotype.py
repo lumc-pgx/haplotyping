d4c607138c2015cc347280bdb971a9d15c00ceb7
Guy Allard <w.g.allard@lumc.nl>
2017-08-08 09:54:30 +0200

    refactoring - split rules out for including in other workflows

--- scripts/pick_haplotype.py
+++ scripts/pick_haplotype.py
@@ -1,6 +1,11 @@
 import json
+import yaml
 from math import isclose
- 
+
+with open(snakemake.input.gene, "r") as infile:
+    gene = yaml.safe_load(infile)
+default = gene["haplotypes"][0]["type"]
+
 def pick(allele):
     assignment = []
     if len(allele["haplotypes"]) > 0:
@@ -10,12 +15,12 @@ def pick(allele):
         assignment = [h["haplotype"] for h in allele["haplotypes"] if h["fraction"] == 1 and isclose(h["jaccard"], top_jaccard) and len(h["significant"]) == top_significant]
     
     if len(assignment) == 0:
-        assignment = [snakemake.params.default]
+        assignment = [default]
 
     return assignment
 
 
-with open(snakemake.input[0], 'r') as infile, open(snakemake.output[0], "w") as outfile:
+with open(snakemake.input.matches, 'r') as infile, open(snakemake.output[0], "w") as outfile:
     alleles = json.load(infile)
     results = ["\t".join([a["sequence_id"]] + pick(a)) for a in alleles]
     print("\n".join(results), file=outfile)
