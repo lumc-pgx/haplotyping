5b042a0ccdbdbb87e7b367a006a6524bb019b691
Guy Allard <w.g.allard@lumc.nl>
2017-12-01 12:20:41 +0100

    use updated variant representation

--- scripts/haplotype_summary.py
+++ scripts/haplotype_summary.py
@@ -4,14 +4,24 @@ def tabulate_allele_matches(allele):
     """
     Convert allele match information into a tabular representation for printing
     """
+    g_notation = lambda variant: variant["g_notation"]
+    is_significant = lambda variant: "significant" in variant["tags"]
+    is_known = lambda variant: "known" in variant["tags"]
+    is_novel = lambda variant: "novel" in variant["tags"]
+
+    variants = allele["variants"]
+    known_variants = [v for v in variants if is_known(v)]
+    novel_variants = [v for v in variants if is_novel(v)]
+    significant_variants = [v for v in variants if is_significant(v)]
+
     overview = "\n".join(
         [
             "- {}".format(allele["sequence_id"]),
             "- Variants: {}    Known: {}    Novel: {}    Significant: {}".format(
-                len(allele["known_variants"]) + len(allele["novel_variants"]),
-                len(allele["known_variants"]),
-                len(allele["novel_variants"]),
-                len(allele["significant_variants"])
+                len(variants),
+                len(known_variants),
+                len(novel_variants),
+                len(significant_variants)
             )
         ]
     )
@@ -19,12 +29,13 @@ def tabulate_allele_matches(allele):
     hits = allele["haplotypes"]
     columns = []
     columns.append(["{:25}".format("Variant")] + \
-                   ["{:25}".format(v) for v in allele["known_variants"]] + \
+                   ["{:25}".format(g_notation(v)) for v in known_variants] + \
                    ["{:25}".format(label) for label in ["Fraction", "Proportion", "Jaccard", "Significant"]])
 
     for hit in hits:
         column = ["{:6}".format(hit["haplotype"])]
-        column += ["{:6}".format("$" if v in allele["significant_variants"] and v in hit["haplotype_variants"] else ("*" if v in hit["haplotype_variants"] else "")) for v in allele["known_variants"]]
+        column += ["{:6}".format("$" if is_significant(v) and g_notation(v) in hit["haplotype_variants"] else \
+                                     ("*" if g_notation(v) in hit["haplotype_variants"] else "")) for v in known_variants]
         column.append("{:6}".format("{}/{}".format(len(hit["shared_variants"]), len(hit["haplotype_variants"]))))
         column.append("{:6}".format(str(hit["fraction"])[:4]))
         column.append("{:6}".format(str(hit["jaccard"])[:4]))
